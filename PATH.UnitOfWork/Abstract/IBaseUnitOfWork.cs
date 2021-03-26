25100e7c354b876adea7fce6caf23a4f8758157e
mburak.kayabal@e-kalite.com.tr <mburak.kayabal@e-kalite.com.tr>
2021-03-26 11:15:40 +0300

    Fixes #3

---
+++ PATH.UnitOfWork/Abstract/IBaseUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using PATH.DataAccessLayer.Abstract;
+using PATH.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PATH.UnitOfWork.Abstract
+{
+    public interface IBaseUnitOfWork : IDisposable
+    {
+        IChatRoomLogDal<ChatRoomLog> ChatRoomLog { get; }
+        int SaveChanges();
+    }
+}
---
+++ PATH.UnitOfWork/Concrete/BaseUnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PATH.DataAccessLayer.Abstract;
+using PATH.DataAccessLayer.Concrete;
+using PATH.Entity;
+using PATH.UnitOfWork.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PATH.UnitOfWork.Concrete
+{
+    public class BaseUnitOfWork : IBaseUnitOfWork
+    {
+        public IChatRoomLogDal<ChatRoomLog> ChatRoomLog => new ChatRoomLogDal(dbContext);
+
+        public int SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(dbContext);
+        }
+
+        private static DbContext dbContext
+        {
+            get
+            {
+                return new PATHCHATContext();
+            }
+        }
+    }
+}
