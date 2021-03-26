bc3c2b028691f91d3a83f7f57329e07e5b3b717e
mburak.kayabal@e-kalite.com.tr <mburak.kayabal@e-kalite.com.tr>
2021-03-26 11:09:39 +0300

    Fixes #2

---
+++ PATH.DataAccessLayer/Abstract/IChatRoomLogDal.cs
@@ -0,0 +1,14 @@
+﻿using PATH.Repository.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PATH.DataAccessLayer.Abstract
+{
+    public interface IChatRoomLogDal<T> : IBaseRepository<T>
+        where T : class, new()
+    {
+    }
+}
